ca6a22b59a7227816f302214a989279677bb4077
Koshkarov Vitaliy <koshkarovvitaly123@gmail.com>
2019-12-25 12:48:28 +0300

    Dependency Injection by Spring

--- src/main/java/ru/koshkarovvitaliy/DemoSpring.java
+++ src/main/java/ru/koshkarovvitaliy/DemoSpring.java
@@ -8,10 +8,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class DemoSpring {
   public static void main(String[] args) {
     try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml")) {
-      Music musicBean = context.getBean("musicBean", Music.class);
-
-      MusicPlayer musicPlayerBean = new MusicPlayer(musicBean);
-      musicPlayerBean.playMusic();
+      MusicPlayer mPlayerBean = context.getBean("musicPlayer", MusicPlayer.class);
+      mPlayerBean.playMusic();
     }
   }
 }
